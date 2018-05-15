f0ce036a41b805127d0034c22bb1bf6b4c4ccdf4
Rafaela Gomes de Miranda <rafsbrug@gmail.com>
2018-05-15 07:35:17 -0300

    Sincronização de Aluno com banco de dados

--- php/banco-tcc.php
+++ php/banco-tcc.php
@@ -1,4 +1,5 @@
 <?php
+    /** PROFESSORES */
     function listaProfessores($conexao) {
         $professor = array();
         $resultado = mysqli_query($conexao, "SELECT * FROM Professor");
@@ -10,5 +11,20 @@
 
     function insereProfessor($conexao,$nome, $titulacao) {
         $query = "INSERT INTO Professor (nome, titulacao) VALUES ('{$nome}','{$titulacao}')";
+        return mysqli_query($conexao, $query);
+    }
+
+    /** ALUNOS */
+    function listaAlunos($conexao) {
+        $aluno = array();
+        $resultado = mysqli_query($conexao, "SELECT * FROM Aluno");
+        while($alunos = mysqli_fetch_assoc($resultado)) {
+            array_push($aluno, $alunos);
+        }
+        return $aluno;
+    }
+
+    function insereAluno($conexao, $ra, $tituloTrabalho, $nome, $orientador, $areaPesquisa, $curso, $email) {
+        $query = "INSERT INTO Aluno (ra, tituloTrabalho, nome, orientador, areaPesquisa, curso, email) VALUES ('{$ra}','{$tituloTrabalho}', '{$nome}','{$orientador}','{$areaPesquisa}','{$curso}','{$email}')";
         return mysqli_query($conexao, $query);
     }
